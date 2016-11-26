d6a61fd691a223a6e97828480004bb19687ae2e0
wdh <2623803199@qq.com>
2016-11-26 16:15:28 +0800

    bbb

--- src/main/java/com/wisezone/crm/constant/Constant.java
+++ src/main/java/com/wisezone/crm/constant/Constant.java
@@ -4,7 +4,7 @@ public class Constant
 {
 	public final static int RESULT_OK=1;
 	public final static int RESULT_ERROR=0;
-	//hello bbb
+	//hello 222
 	public final static String OPT_SUCCESS="操作成功";
 	public final static String OPT_FAILURE="操作失败,请联系管理员";
 	public final static String QUERY_FAILURE="查询失败，请联系管理员";
